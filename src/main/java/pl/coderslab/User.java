553a6d43a3a3611bca7d2a5f7ed78b30b1d0ae1e
PiotrSciepko <piotrek.priv@gmail.com>
2020-10-17 19:33:08 +0200

    add User class constructors

--- src/main/java/pl/coderslab/User.java
+++ src/main/java/pl/coderslab/User.java
@@ -7,6 +7,15 @@ public class User {
     private String email;
     private String password;
 
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
     public int getId() {
         return id;
     }
